811674b4c2870e04e5b6cf356c8dd406d0c6f103
Meriem <meriem.barka@gmail.com>
2023-12-13 23:21:55 +0100

    dedlete_folder

--- backend/dist/auth/auth.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { AuthService } from "./auth.service";
-export declare class AuthController {
-    private readonly authService;
-    constructor(authService: AuthService);
-    register(user: any): Promise<void>;
-    login(credentials: any): Promise<void>;
-}
--- backend/dist/auth/auth.service.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class AuthService {
-    register(user: any): Promise<void>;
-    login(credentials: any): Promise<void>;
-}
